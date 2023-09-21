5f513400bc6a83242ff62052ca6a96204810b712
FaizanAsifB <faizanasif.dev@gmail.com>
2023-09-21 19:31:00 +0200

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '21d27c60-de9e-4d59-aecb-ae059f4b23e8';
+export const MODAL_CLOSE_SEC = 2.5;
